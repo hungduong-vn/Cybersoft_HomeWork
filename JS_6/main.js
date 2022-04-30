5cda8c4544ebdafcbaf9473f4ec507caa0a3d8ff
Jasper-Duong <duongngochung13@gmail.com>
2022-04-30 12:14:29 +0700

    Finished extra exercises

--- JS_6/main.js
+++ JS_6/main.js
@@ -15,8 +15,8 @@ document.getElementById("run2").onclick = function () {
   var x = document.getElementById("input2_1").value * 1;
   var n = document.getElementById("input2_2").value * 1;
   var result = 0;
-  for (var i=1; i<=n; i++){
-    result += x**i;
+  for (var i = 1; i <= n; i++) {
+    result += x ** i;
   }
   document.getElementById("output2").innerHTML = result;
   document.getElementById("output2").classList.add("showResult");
@@ -26,33 +26,62 @@ document.getElementById("run3").onclick = function () {
   var n = document.getElementById("input3_1").value * 1;
 
   var gt = 1;
-  for (var i=1; i<=n; i++){
+  for (var i = 1; i <= n; i++) {
     gt *= i;
   }
   document.getElementById("output3").innerHTML = gt;
   document.getElementById("output3").classList.add("showResult");
 };
 //Ex4
-function lastChild(list){
-  return list.children[list.children.length-1];
+function lastChild(list) {
+  return list.children[list.children.length - 1];
 }
 document.getElementById("run4").onclick = function () {
   var outputText = document.getElementById("output4_1");
   for (i = 1; i <= 10; i++) {
-    outputText.appendChild(document.createElement('div'));
+    outputText.appendChild(document.createElement("div"));
     var div = lastChild(outputText);
-    div.style.width = '100%';
-    div.style.height = '50px';
+    div.style.width = "100%";
+    div.style.height = "50px";
     if (i % 2 === 0) {
-      div.style.backgroundColor = 'red';
+      div.style.backgroundColor = "red";
     } else {
-      div.style.backgroundColor = 'blue';
+      div.style.backgroundColor = "blue";
     }
     div.innerHTML = i;
-    div.style.textAlign = 'center';
-    div.style.lineHeight = '50px';
-    div.style.color = 'white';
+    div.style.textAlign = "center";
+    div.style.lineHeight = "50px";
+    div.style.color = "white";
   }
   console.log(outputText);
   document.getElementById("output4_1").classList.add("showResult");
+};
+//Ex5
+document.getElementById("run5").onclick = function () {
+  var n = document.getElementById("input5_1").value * 1;
+
+  var result = "";
+  if (n < 2) {
+    result = "";
+  } else {
+    for (var i = 2; i <= n; i++) {
+      if (n === 2) {
+        result += "2 ";
+        break;
+      }
+      var isPrime = true; //Not prime number
+      for (var j = 2; j < i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        result += i + " ";
+      }
+    }
+  }
+
+  document.getElementById("output5_1").innerHTML = result;
+  document.getElementById("output5_1").classList.add("showResult");
 };
