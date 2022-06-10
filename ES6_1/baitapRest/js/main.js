133ca43c29333f34ec8004946075b32f825eb78b
Jasper-Duong <duongngochung13@gmail.com>
2022-06-10 18:11:08 +0700

    Edit function accepting Rest Parameter

--- ES6_1/baitapRest/js/main.js
+++ ES6_1/baitapRest/js/main.js
@@ -3,7 +3,7 @@ let getEle = (id) => document.getElementById(id);
 const inpK1 = ["inpToan", "inpLy", "inpHoa"];
 const inpK2 = ["inpVan", "inpSu", "inpDia", "inpEnglish"];
 
-let tinhDTB = (arrDiem) =>
+let tinhDTB = (...arrDiem) =>
   arrDiem.reduce((pre, next) => pre + next, 0) / arrDiem.length;
 
 let getDiemInp = (inpIDArr) => inpIDArr.map((ele) => getEle(ele).value * 1);
@@ -12,7 +12,7 @@ let printDTB = (divID, DTB) => getEle(divID).innerHTML = DTB;
 
 let run = (inpIDArr,divKQ) => {
   let inp = getDiemInp(inpIDArr);
-  let DTB = tinhDTB(inp);
+  let DTB = tinhDTB(...inp);
   printDTB(divKQ,DTB);
 }
 
