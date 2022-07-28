14b15059411bc93a2cc2fa50d1f82cf4aaece7cb
Jasper-Duong <duongngochung13@gmail.com>
2022-07-28 10:21:19 +0700

    Fix bugs action creator

--- react_5/src/Components/BaiTapQuanLyNguoiDung/RegisterForm.jsx
+++ react_5/src/Components/BaiTapQuanLyNguoiDung/RegisterForm.jsx
@@ -3,49 +3,78 @@ import { useDispatch } from "react-redux";
 import { addUserAction } from "../../Store/actions/user";
 
 export default function RegisterForm() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
     <div className="card p-0">
       <div className="card-header bg-warning text-white font-weight-bold">
         REGISTER FORM
       </div>
       <div className="card-body">
-        <form >
+        <form>
           <div className="row">
             <div className="col-6">
               <div className="form-group">
                 <label>Username</label>
-                <input title="Tên Tài Khoản" name="username" type="text" className="form-control" />
+                <input
+                  title="Tên Tài Khoản"
+                  name="username"
+                  type="text"
+                  className="form-control"
+                />
               </div>
             </div>
             <div className="col-6">
               <div className="form-group">
                 <label>Full Name</label>
-                <input title="Họ và Tên" name="fullName" type="text" className="form-control" />
+                <input
+                  title="Họ và Tên"
+                  name="fullName"
+                  type="text"
+                  className="form-control"
+                />
               </div>
             </div>
             <div className="col-6">
               <div className="form-group">
                 <label>Password</label>
-                <input title="Mật Khẩu" name="password" type="text" className="form-control" />
+                <input
+                  title="Mật Khẩu"
+                  name="password"
+                  type="text"
+                  className="form-control"
+                />
               </div>
             </div>
             <div className="col-6">
               <div className="form-group">
                 <label>Phone Number</label>
-                <input title="Số Điện Thoại" name="phoneNumber" type="text" className="form-control" />
+                <input
+                  title="Số Điện Thoại"
+                  name="phoneNumber"
+                  type="text"
+                  className="form-control"
+                />
               </div>
             </div>
             <div className="col-6">
               <div className="form-group">
                 <label>Email</label>
-                <input title="Email" name="email" type="text" className="form-control" />
+                <input
+                  title="Email"
+                  name="email"
+                  type="text"
+                  className="form-control"
+                />
               </div>
             </div>
             <div className="col-6">
               <div className="form-group">
                 <label>Type</label>
-                <select title="Loại Tài Khoản" name="type" className="form-control">
+                <select
+                  title="Loại Tài Khoản"
+                  name="type"
+                  className="form-control"
+                >
                   <option>Client</option>
                   <option>Admin</option>
                 </select>
@@ -55,9 +84,14 @@ export default function RegisterForm() {
         </form>
       </div>
       <div className="card-footer text-muted">
-        <button className="btn btn-warning mr-2" onClick={() => {
-          dispatch(addUserAction())
-        }}>SAVE</button>
+        <button
+          className="btn btn-warning mr-2"
+          onClick={() => {
+            dispatch(addUserAction());
+          }}
+        >
+          SAVE
+        </button>
         <button className="btn btn-outline-dark">RESET</button>
       </div>
     </div>
--- react_5/src/Store/actions/user.js
+++ react_5/src/Store/actions/user.js
@@ -5,20 +5,9 @@ import {
   SELECT_USER,
 } from "./../types/user";
 
-const addUserAction = (user) => {
-  return { type: ADD_USER, payload: user };
-};
-
-const selectUserAction = (id) => {
-  return { type: SELECT_USER, payload: id };
-};
-
-const updateUserAction = (user) => {
-  return { type: UPDATE_USER, payload: user };
-};
-
-const deleteUserAction = (id) => {
-  return { type: DELETE_USER, payload: id };
-};
+const addUserAction = (user) => ({ type: ADD_USER, payload: user });
+const selectUserAction = (id) => ({ type: SELECT_USER, payload: id });
+const updateUserAction = (user) => ({ type: UPDATE_USER, payload: user });
+const deleteUserAction = (id) => ({ type: DELETE_USER, payload: id });
 
 export { addUserAction, selectUserAction, updateUserAction, deleteUserAction };
